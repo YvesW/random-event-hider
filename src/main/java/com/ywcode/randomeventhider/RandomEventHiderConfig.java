ad2c277a67f139f254cb1688fd2c5e480702be4e
YvesW <7929021+YvesW@users.noreply.github.com>
2024-08-26 00:08:19 +0200

    Config: reduce amount of wildcard imports

--- src/main/java/com/ywcode/randomeventhider/RandomEventHiderConfig.java
+++ src/main/java/com/ywcode/randomeventhider/RandomEventHiderConfig.java
@@ -1,6 +1,9 @@
 package com.ywcode.randomeventhider;
 
-import net.runelite.client.config.*;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("RandomEventHider")
 public interface RandomEventHiderConfig extends Config
